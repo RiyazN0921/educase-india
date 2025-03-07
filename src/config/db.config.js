b76eaec3b3e01f0526be3cc7deb91cb36993e2aa
RiyazN0921 <riyazn886@gmail.com>
2025-03-07 14:56:55 +0530

    database code updated

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -3,11 +3,15 @@ const { Sequelize } = require('sequelize')
 require('dotenv').config()
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
+  // process.env.DB_NAME,
+  'freedb_educase',
+  // process.env.DB_USER,
+  'freedb_educase',
+  // process.env.DB_PASS,
+  'q6j@zrPd?Zx$gEY',
   {
-    host: process.env.DB_HOST,
+    // host: process.env.DB_HOST,
+    host: 'sql.freedb.tech',
     dialect: 'mysql',
     port: 3306,
     logging: false,
